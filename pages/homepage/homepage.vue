33225ddb15cb6769aa8ab4eb7a59a43eeb76667b
mitsu <zichuan_huang@qq.com>
2023-07-13 17:25:53 +0800

    完成用户基本信息区域的界面开发

--- pages/homepage/homepage.vue
+++ pages/homepage/homepage.vue
@@ -1,18 +1,24 @@
 <template>
-	<view>
+	<view class="body">
 		<!-- 用户基本信息区域 -->
 		<view class="user-info-container">
 			<!-- 用户头像 -->
 			<view class="user-avatar-box">
-				
+				<img id="img" src="/static/user_icon.jpg">
 			</view>
 			<!-- 用户名称、注册时间 -->
 			<view class="user-info-box">
-				
+				<view class="user-name">
+					用户名
+				</view>
+				<view class="register-time">
+					注册时间：2023-06-20
+				</view>
 			</view>
 			<!-- 用户二维码 -->
 			<view class="user-qrcode-box">
-				
+				<img id="qrcode" src="/static/qrcode_img.png">
+				<uni-icons type="right"></uni-icons>
 			</view>
 		</view>
 		<!-- 选项设置区域 -->
@@ -44,5 +50,71 @@
 </script>
 
 <style lang="scss">
-
+	.body {
+		background-color: #f1f1f1;
+	}
+	
+	.user-info-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #ffffff;
+		border-top: 1px solid #aaa;
+		border-bottom: 1px solid #aaa;
+		margin-bottom: 17px;
+	}
+	.user-avatar-box {
+		position: relative;
+		left: 0%;
+		border: 1px solid #000;
+		border-radius: 5px;
+		overflow: hidden;
+		width: 88px;
+		height: 88px;
+		margin: 32px 10px 32px 16px;
+		
+		#img {
+			max-width: 100%;
+			max-height: 100%;
+			display: block;
+			margin: auto;
+		}
+	}
+	
+	.user-info-box {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		
+		.user-name {
+			color: #000;
+			font-size: 23px;
+			font-style: normal;
+			font-weight: 400;
+			line-height: normal;
+			margin-bottom: 18px;
+		}
+		
+		.register-time {
+			color: #000;
+			font-family: Inter;
+			font-size: 13px;
+			font-style: normal;
+			font-weight: 400;
+			line-height: normal;
+		}
+	}
+	
+	.user-qrcode-box {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		
+		#qrcode {
+			width: 20px;
+			height: 20px;
+			margin-left: 75px;
+			margin-right: 5px;
+		}
+	}
 </style>
