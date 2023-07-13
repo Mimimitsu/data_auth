2aad8fbdf084a0c8880a85f850654f5c49be408f
mitsu <zichuan_huang@qq.com>
2023-07-13 16:10:03 +0800

    创建用户主页，完成界面框架编写

--- pages/homepage/homepage.vue
+++ pages/homepage/homepage.vue
@@ -1,6 +1,35 @@
 <template>
 	<view>
-		用户登录主页
+		<!-- 用户基本信息区域 -->
+		<view class="user-info-container">
+			<!-- 用户头像 -->
+			<view class="user-avatar-box">
+				
+			</view>
+			<!-- 用户名称、注册时间 -->
+			<view class="user-info-box">
+				
+			</view>
+			<!-- 用户二维码 -->
+			<view class="user-qrcode-box">
+				
+			</view>
+		</view>
+		<!-- 选项设置区域 -->
+		<view class="option-container">
+			<view class="option-item">
+				授权消息
+			</view>
+			<view class="option-item">
+				授权记录
+			</view>
+			<view class="option-item">
+				个人信息
+			</view>
+			<view class="option-item">
+				设置
+			</view>
+		</view>
 	</view>
 </template>
 
